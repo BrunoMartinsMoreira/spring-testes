268a4cbfa388ed471c3cbfc6cac3a4b6e48d061b
Bruno M Moreira <bruno_moreira.19@outlook.com>
2025-06-08 16:42:18 -0300

    feat: altera a chamada para cadastro de usuarios para a camada de services

--- src/main/java/br/com/moreira/hellospring/controllers/ProdutoController.java
+++ src/main/java/br/com/moreira/hellospring/controllers/ProdutoController.java
@@ -3,6 +3,8 @@ package br.com.moreira.hellospring.controllers;
 import br.com.moreira.hellospring.exceptions.ProdutoDuplicadoException;
 import br.com.moreira.hellospring.exceptions.ProdutoNotFountException;
 import br.com.moreira.hellospring.models.Produto;
+import br.com.moreira.hellospring.services.IProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,36 +13,47 @@ import java.util.ArrayList;
 
 @RestController
 public class ProdutoController {
-  private final ArrayList<Produto> produtos = new ArrayList<>();
+  @Autowired
+  private IProdutoService produtoService;
 
   @PostMapping("/produtos")
   public ResponseEntity<Produto> criarProduto(@RequestBody Produto prod) {
-    boolean produtoExiste = produtos.stream()
-            .anyMatch(p -> p.getId().equals(prod.getId()));
-    if(produtoExiste) {
-      throw new ProdutoDuplicadoException(prod.getId());
+    try {
+      Produto produtoCriado = this.produtoService.criarProduto(prod);
+
+      return ResponseEntity
+          .status(HttpStatus.CREATED)
+          .body(produtoCriado);
+    }catch (ProdutoDuplicadoException e) {
+      throw e;
     }
-
-    produtos.add(prod);
-
-    return ResponseEntity
-        .status(HttpStatus.CREATED)
-        .body(prod);
   }
 
   @GetMapping("/produtos/{id}")
   public ResponseEntity<Produto> getProduto(@PathVariable Integer id) {
-    Produto prod = produtos
-        .stream()
-        .filter(p -> p.getId().equals(id))
-        .findFirst()
-        .orElseThrow(() ->  new ProdutoNotFountException(id));
-
-    return ResponseEntity.status(HttpStatus.OK).body(prod);
+   try {
+     Produto prod = this.produtoService.getProduto(id);
+     return ResponseEntity.status(HttpStatus.OK).body(prod);
+   } catch (ProdutoNotFountException e) {
+     throw e;
+   }
   }
 
   @GetMapping("/produtos")
   public ResponseEntity<ArrayList<Produto>> getProdutos(){
-    return ResponseEntity.status(HttpStatus.OK).body(produtos);
+    return ResponseEntity.status(HttpStatus.OK).body(this.produtoService.getProdutos());
+  }
+
+  @PatchMapping("/produtos/{id}/desconto/{percentual}")
+  public  ResponseEntity<Produto> aplicarDesconto(
+      @PathVariable Integer id,
+      @PathVariable Double percentual
+  ) {
+    try {
+     Produto prod = this.produtoService.aplicarDesconto(id, percentual);
+     return ResponseEntity.status(HttpStatus.OK).body(prod);
+    } catch (ProdutoNotFountException e) {
+      throw e;
+    }
   }
 }
